7fc785787ce92072eb9ccff23ce211e881870659
Kamogelo <mmopanemoroka@gmail.com>
2025-06-21 16:43:03 +0200

    db connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -11,5 +12,6 @@ const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
+  connectDB();
 });
